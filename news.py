15ff6c5b2a470ab7342a09af1d1d2552ba81dc3c
Pachara S <pachara.s@mail.kmutt.ac.th>
2015-08-26 13:46:48 +0700

    Add function checkNews
    checkNews is storing rss to json files

--- news.py
+++ news.py
@@ -2,9 +2,16 @@
 # -*- coding: UTF-8 -*-
 import feedparser
 from HTMLParser import HTMLParser
+from time import sleep
+import pprint
+import json
 
 link = 'https://www.blognone.com/atom.xml'
 x = feedparser.parse(link)
+data = open('data.json')
+y = json.load(data)
+feedData = {}
+
 
 class MLStripper(HTMLParser):
     def __init__(self):
@@ -23,14 +30,36 @@ def strip_tags(html):
 def getNews():
     for x.entrie in x.entries:
         #print len(x.entries)
-        print x.entrie['link'],x.entrie['guid']
+        print x.entrie['guid'].split()[0]
+        print x.entrie['link']
         print x.entrie['title'],'\n'
-        #text = strip_tags(x.entrie['description'])
-        #text = text.strip()
-        #print text
-#print x.entries[0].summary
+        #sleep(0.15)
+        #feedData[id] = {'link': link,'title': title}
+#       feedData = {'id': id,'link': link,'title': title}
+        #pprint.pprint(feedData)
+        text = strip_tags(x.entrie['description'])
+        text = text.strip()
+        print text
+    #print x.entries[0]x.summaryx
 
-getNews()
+def checkNews():
+    for x.entrie in x.entries:
+        #print len(x.entries)
+        id = x.entrie['guid'].split()[0]
+        link = x.entrie['link']
+        title = x.entrie['title'],'\n'
+        #sleep(0.15)
+        feedData['result'] = {'id': id,'link': link,'title': title}
+#       feedData = {'id': id,'link': link,'title': title}
+    pprint.pprint(feedData)
+    with open('data.json', 'w') as outfile:
+        json.dump(feedData, outfile)
+
+
+
+
+checkNews()
+#getNews()
 
 
 
